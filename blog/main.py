e30452ba0cac6b8979a85b12cd59dac08900c4f1
KimJaeGeun <jgk9612@gmail.com>
2022-06-05 21:18:51 +0900

    feat: Add Output Menu code

--- blog/main.py
+++ blog/main.py
@@ -1,9 +1,10 @@
 import os
 import csv
+from secrets import choice
 from post import Post
 
 # data.csv 파일 경로
-file_path = "./blog/data.csv"
+file_path = "./data.csv"
 
 # post 객체를 저장할 리스트 선언
 post_list = []
@@ -23,6 +24,18 @@ else:
     f = open(file_path, "w", encoding="utf8", )
     f.close()
 
-# 출력 Test
-print(post_list[0].get_title())
-print(post_list[1].get_title())
+# 메뉴 출력하기
+while True:
+    print("\n\n- My BLOG -")
+    print("- 메뉴를 선택해 주세요 -")
+    print("1. 게시글 쓰기")
+    print("2. 게시글 목록")
+    print("3. 프로그램 종료")
+    choice = int(input(">>>"))
+    if choice == 1:
+        print("게시글 쓰기")
+    elif choice == 2:
+        print("게시글 목록")
+    elif choice == 3:
+        print("프로그램 종료")
+        break
