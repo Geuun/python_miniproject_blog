72bb59d13285a717712a5aba04d5b4a39fa980e2
KimJaeGeun <jgk9612@gmail.com>
2022-06-05 21:23:04 +0900

    feat: Add handling exceptions code

--- blog/main.py
+++ blog/main.py
@@ -31,11 +31,16 @@ while True:
     print("1. 게시글 쓰기")
     print("2. 게시글 목록")
     print("3. 프로그램 종료")
-    choice = int(input(">>>"))
-    if choice == 1:
-        print("게시글 쓰기")
-    elif choice == 2:
-        print("게시글 목록")
-    elif choice == 3:
-        print("프로그램 종료")
-        break
+    # 예외 처리
+    try:
+        choice = int(input(">>>"))
+    except ValueError:
+        print("숫자를 입력해 주세요.")
+    else:
+        if choice == 1:
+            print("게시글 쓰기")
+        elif choice == 2:
+            print("게시글 목록")
+        elif choice == 3:
+            print("프로그램 종료")
+            break
